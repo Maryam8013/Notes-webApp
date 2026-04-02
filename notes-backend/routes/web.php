57c8e9b0f4d1692af082dcf4e95e4594f21eafa1
maryamirfan123 <maryamirfan1036@gmail.com>
2026-04-02 12:08:21 +0500

    metrics added

--- notes-backend/routes/web.php
+++ notes-backend/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/metrics', function () {
+    $metrics = "app_requests_total 1\n";
+    return response($metrics, 200)
+        ->header('Content-Type', 'text/plain');
+});
